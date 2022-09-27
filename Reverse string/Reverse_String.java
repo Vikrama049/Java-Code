2f0a775053b2a006324e26421658d125a4aadeee
Vikrama049 <98575136+Vikrama049@users.noreply.github.com>
2022-09-27 13:42:59 +0530

    Add files via upload

---
+++ Reverse string/Reverse_String.java
@@ -0,0 +1,15 @@
+package String_codes;
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		String s1 = "Aditya Vikram d Kuamr";
+		
+		for(int i=s1.length()-1;i>=0;i--)		
+		{
+			System.out.print(s1.charAt(i));
+		}
+
+	}
+
+}
