615ed188c8f1d47c6cbc7378e4564bfba9a4680e
Vikrama049 <98575136+Vikrama049@users.noreply.github.com>
2022-10-06 19:16:42 +0530

    Add files via upload

---
+++ Lab (6-10-22)/Practical_4/Child.java
@@ -0,0 +1,17 @@
+package Practical_4;
+
+public class Child extends Parents{
+
+	void Speak()	{
+		
+		System.out.println("I can speak 'Marathi and English' Language");
+		
+	}
+	
+	void Dance()	{
+		
+		System.out.println("I can dance ");
+		
+	}
+
+}
---
+++ Lab (6-10-22)/Practical_4/Parents.java
@@ -0,0 +1,11 @@
+package Practical_4;
+
+public class Parents {
+
+	void Singing()	{
+		
+		System.out.println("I am singing a song");
+	
+	}
+
+}
