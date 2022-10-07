e846de1b1ab9473c2237ea3603a14e090fe84d69
Vikrama049 <98575136+Vikrama049@users.noreply.github.com>
2022-10-07 11:00:23 +0530

    Add files via upload

---
+++ Class Practicals/Encapsulation/Bike.java
@@ -0,0 +1,27 @@
+/* Program: Program for encapsulation
+ * Name: Aditya Vikram
+ * Date : 7/10/22
+ */
+
+package Encapsulation;
+
+class Bike {
+	
+	private String Company_Name;
+	private int Bike_No;
+	
+	//creating getters and setters methods
+	public String getCompany_Name() {
+		return Company_Name;
+	}
+	public void setCompany_Name(String company_Name) {
+		Company_Name = company_Name;
+	}
+	public int getBike_No() {
+		return Bike_No;
+	}
+	public void setBike_No(int bike_No) {
+		Bike_No = bike_No;
+	}
+
+}
