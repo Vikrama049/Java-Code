9c316f27699fcb05e614790b5a4d71d5e9c457c5
Vikrama049 <98575136+Vikrama049@users.noreply.github.com>
2022-10-13 14:16:13 +0530

    Update Stars.java

--- Lab (13-10-22)/Stars.java
+++ Lab (13-10-22)/Stars.java
@@ -1,9 +1,15 @@
+/* Program to demontrate on nested For Loop and printing Triangle
+* Name : Aditya Vikram
+* Date : 13/10/22
+*/
+
 package Lab_13102022;
 
 public class Stars {
 
 	public static void main(String[] args) {
 		
+		//Using for Loop
 		for(int i=1;i<=6;i++)	{
 			System.out.println();
 			for(int j=1;j<=i;j++)	{
