a83ca4838b244cd2cba6bef727c83ff42e7034a8
Vikrama049 <98575136+Vikrama049@users.noreply.github.com>
2022-10-13 14:15:05 +0530

    Update Positive_Numbers.java

--- Lab (13-10-22)/Positive_Numbers.java
+++ Lab (13-10-22)/Positive_Numbers.java
@@ -1,14 +1,21 @@
+/* Program to demontrate on nested For Loop and printing Triangle
+* Name : Aditya Vikram
+* Date : 13/10/22
+*/
+
 package Lab_13102022;
 
 
 public class Positive_Numbers {
 
 	public static void main(String[] args) {
-
+		
+		//Importing Scanner 
 		java.util.Scanner sc = new java.util.Scanner(System.in);
 	
 		int a = 0;
 	    int sum = 0;
+		
 	    do {
 	    System.out.print("Enter a number = ");
 		a = sc.nextInt();
@@ -17,6 +24,7 @@ public class Positive_Numbers {
 	    	sum = sum+ a;
 	    }
 	      
+		    //Using while Loop
 	    } while(a >= 0); 
 		   
 	    System.out.println();
