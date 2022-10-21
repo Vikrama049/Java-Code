6906e8d31229b8bdfba0f8b303c6db56efd34d1b
Vikrama049 <98575136+Vikrama049@users.noreply.github.com>
2022-10-21 11:46:47 +0530

    Add files via upload

---
+++ Hands on/Array/Reverse_of_String.java
@@ -0,0 +1,35 @@
+/* Program to find Reverse of String
+ * Name : Aditya Vikram
+ * Date : 21-10-22
+ */
+
+package Array;
+
+//Importing Scanner
+import java.util.Scanner;
+
+//import java.util.*;
+public class Reverse_of_String {
+	
+	public static void main(String ... args )
+
+	{
+		Scanner sc = new Scanner(System.in);
+		//Taking input from user
+		System.out.print("Write Any word : ");
+		String a = sc.nextLine();
+		char[] b= a.toCharArray();
+		
+		System.out.println();
+		
+		//Printing values of String in reverse
+		System.out.print("Reverse String is : [");
+		
+		//Using for loop
+		for(int i=b.length-1;i>=0;i--)
+		{
+			System.out.print(b[i]+" ");
+		}
+		System.out.println("]");
+	}
+}
